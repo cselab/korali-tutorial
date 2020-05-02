ae3be22b854c815f165dd0fc26d060bd1090bd0f
Sergio Martin <martiser@ethz.ch>
2020-05-02 13:55:01 +0200

    Adding first practice

---
+++ practice2/practice2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Define Objective Function
+def myObjectiveFunction(sample):
+
+# Import and Start Korali's Engine
+import korali
+k = korali.Engine()
+
+# Creating new experiment
+e = korali.Experiment()
+
+# Configuring Problem
+
+# Defining the problem's variables.
+
+# Configure the CMAES solver
+
+# Add Termination Criteria
+
+# Running Korali
+
